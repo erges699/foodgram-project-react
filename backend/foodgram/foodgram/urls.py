96bc04941db4e0655b4b9d72ff19a15fe25f7660
Serge <erges699@gmail.com>
2022-07-31 16:02:05 +0300

    Serializ

--- backend/foodgram/foodgram/urls.py
+++ backend/foodgram/foodgram/urls.py
@@ -3,7 +3,5 @@ from django.urls import include, path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('auth/', include('users.urls', namespace='users')),
     path('api/', include('api.urls')),
-    path('receipes/', include('receipes.urls')),
 ]
