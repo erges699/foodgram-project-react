2c96afa900fd9446c27d7fb7d1b80aa0bb721c42
Serge <erges699@gmail.com>
2022-08-13 16:19:17 +0300

    22020813-03

--- backend/foodgram/users/views.py
+++ backend/foodgram/users/views.py
@@ -1,31 +1,59 @@
 from django.contrib.auth import get_user_model
+from django.http import HttpResponse
 from django.shortcuts import get_object_or_404
+from django.db.models import Sum
 from djoser.views import UserViewSet
 from rest_framework import status
-from rest_framework.decorators import action
+from rest_framework.decorators import action, api_view, permission_classes
 from rest_framework.generics import ListAPIView
-from rest_framework.permissions import IsAuthenticated
+# from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 
 from .paginators import SmallPageNumberPagination
-from recipes.models import Follow
-from api.serializers import (CreateFollowSerializer, ShowFollowsSerializer,
+from recipes.models import Follow, IngredientInRecipe
+from api.serializers import (FollowSerializer, ShowFollowsSerializer,
                              UserSerializer, FollowerSerializer)
 
 User = get_user_model()
+
+
+def shopping_cart_file(user):
+    ingredients = IngredientInRecipe.objects.filter(
+        recipe__buying__user=user).values(
+        'ingredient__name',
+        'ingredient__unit').annotate(total=Sum('amount'))
+
+    shopping_list = 'список:\n'
+    for number, ingredient in enumerate(ingredients, start=1):
+        shopping_list += (
+            f'{number} '
+            f'{ingredient["ingredient__name"]} - '
+            f'{ingredient["total"]} '
+            f'{ingredient["ingredient__unit"]}\n')
+    return shopping_list
+
+
+@api_view(['GET', ])
+# @permission_classes([permissions.IsAuthenticated])
+def download_shopping_cart(request):
+    user = request.user
+    txt_file_output = shopping_cart_file(user)
+    response = HttpResponse(txt_file_output, 'Content-Type: text/plain')
+    response['Content-Disposition'] = (
+        'attachment;' 'filename="Список_покупок.txt"'
+    )
+    return response
 
 
 class UsersViewSet(UserViewSet):
     serializer_class = UserSerializer
     pagination_class = SmallPageNumberPagination
 
-    @action(detail=False, methods=['get'],
-            permission_classes=[IsAuthenticated])
+    @action(detail=False, methods=['get'])
     def me(self, request, *args, **kwargs):
         return super(UsersViewSet, self).me(request, *args, **kwargs)
 
-    @action(detail=False, methods=['get'],
-            permission_classes=[IsAuthenticated])
+    @action(detail=False, methods=['get'])
     def subscriptions(self, request):
         queryset = User.objects.filter(following__user=request.user)
         page = self.paginate_queryset(queryset)
@@ -36,10 +64,9 @@ class UsersViewSet(UserViewSet):
         serializer = self.get_serializer(queryset, many=True)
         return Response(serializer.data, status=status.HTTP_200_OK)
 
-    @action(detail=True, methods=['post'],
-            permission_classes=[IsAuthenticated])
+    @action(detail=True, methods=['post'])
     def subscribe(self, request, id=None):
-        serializer = CreateFollowSerializer(
+        serializer = FollowSerializer(
             data=dict(author=id, user=request.user.id),
             context={'request': request}
         )
@@ -65,7 +92,7 @@ class UsersViewSet(UserViewSet):
 
 class FollowViewSet(ListAPIView):
     serializer_class = ShowFollowsSerializer
-    permission_classes = [IsAuthenticated]
+    # permission_classes = [IsAuthenticated]
     pagination_class = SmallPageNumberPagination
 
     def get_queryset(self):
